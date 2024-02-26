7ee207fff123bf176b0df9588e250b7adf9fbf82
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-02-26 11:13:37 +0100

    created updateDto

--- src/books/dto/update-book.dto.ts
+++ src/books/dto/update-book.dto.ts
@@ -1,4 +1,20 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateBookDto } from './create-book.dto';
+import { IsNotEmpty, IsOptional, IsString, MaxLength } from 'class-validator';
 
-export class UpdateBookDto extends PartialType(CreateBookDto) {}
+export class UpdateBookDto extends PartialType(CreateBookDto) {
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(60)
+  title: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(100)
+  description: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(30)
+  author: string;
+}
