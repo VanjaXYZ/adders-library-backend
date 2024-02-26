edec292f831d47db4e10281528c82a53c15e4e1d
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-02-26 23:51:04 +0100

    add db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MongooseModule } from '@nestjs/mongoose';
 import { BooksModule } from './books/books.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://127.0.0.1/adders-library'), BooksModule],
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule.forRoot()],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        uri: config.get<string>('MONGO_CONNECTION_STRING'), // Loaded from .ENV
+      }),
+    }),
+    BooksModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
