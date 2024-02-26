965f32074dba48d1d44b1e15044d7fec7cb34610
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-02-26 11:13:42 +0100

    created

--- src/books/dto/create-book.dto.ts
+++ src/books/dto/create-book.dto.ts
@@ -1,1 +1,18 @@
-export class CreateBookDto {}
+import { IsNotEmpty, IsOptional, IsString, MaxLength } from 'class-validator';
+
+export class CreateBookDto {
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(60)
+  title: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(100)
+  description: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(30)
+  author: string;
+}
