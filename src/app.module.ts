23c83822bd840d5432ed7ab8dc3bc87a6bd0549a
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-03-05 19:37:54 +0100

    add userModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,9 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BooksModule } from './books/books.module';
-import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
+
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -18,9 +18,10 @@ import { UserModule } from './user/user.module';
       }),
     }),
     BooksModule,
-    AuthModule,
-    UsersModule,
+
     UserModule,
+
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
